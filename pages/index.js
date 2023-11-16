c77234dc4f1b92bf02dcc543c0f7884b1e2e3e15
gabriel <gabriel.lima2307@uol.com.br>
2023-11-16 11:16:10 -0300

    pages ok

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+export default function Home (){
+    return (
+    <div>
+        <h1>Site PWEB</h1>
+        <a class="uncolored-link" href='https://github.com/Lima404/Pweb' target='blank'>Github repositorios das receitas</a>
+        <h2>Receitas NEXT</h2>
+        <a href='https://github.com/Lima404/Pweb/tree/main/next/pages/receita1' target='blank'>Receita 1</a><br></br>
+        <a href='https://github.com/Lima404/Pweb/tree/main/next/pages/receita2' target='blank'>Receita 2</a><br></br>
+        <a href='https://github.com/Lima404/Pweb/tree/main/next/pages/receita3' target='blank'>Receita 3</a>
+    </div>
+    )
+}
